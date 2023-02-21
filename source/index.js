42642a0fb5cc44bc92a20acf8b0d0c90f812ef40
hipnguyen <125984399+hipnguyen@users.noreply.github.com>
2023-02-21 21:12:42 +0700

    Update index.js

--- source/index.js
+++ source/index.js
@@ -1,11 +1,20 @@
-// alert("Xin chào! Đây là một thông báo.");
-if (confirm("Bạn có chắc chắn muốn xóa?")) {
-    // Nếu người dùng nhấn "OK"
-    xoaDuLieu();
-  } else {
-    // Nếu người dùng nhấn "Cancel"
-    khongXoaDuLieu();
-  }
+alert("Henry Nguyen Coming Soon");
+// if (confirm("Bạn có chắc chắn muốn xóa?")) {
+//     // Nếu người dùng nhấn "OK"
+//     xoaDuLieu();
+//   } else {
+//     // Nếu người dùng nhấn "Cancel"
+//     khongXoaDuLieu();
+//   }
 //   let ten = prompt("Hãy nhập tên của bạn:", "Nguyễn Văn A");
 //   alert("Xin chào " + ten + "!");
-    
+$(document).ready(function() {
+  setInterval(function() {
+    var now = new Date();
+    var hours = now.getHours();
+    var minutes = now.getMinutes();
+    var seconds = now.getSeconds();
+    var color = '#' + hours + minutes + seconds;
+    $('body').css('background-color', color);
+  }, 5000); // thay đổi màu sau mỗi 5 giây
+});    
