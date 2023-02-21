4630bbf15fcd5661c6710113460589b140c2dc83
hipnguyen <125984399+hipnguyen@users.noreply.github.com>
2023-02-21 13:36:59 +0000

    Add files via upload

---
+++ source/index.js
@@ -0,0 +1,11 @@
+// alert("Xin chào! Đây là một thông báo.");
+if (confirm("Bạn có chắc chắn muốn xóa?")) {
+    // Nếu người dùng nhấn "OK"
+    xoaDuLieu();
+  } else {
+    // Nếu người dùng nhấn "Cancel"
+    khongXoaDuLieu();
+  }
+//   let ten = prompt("Hãy nhập tên của bạn:", "Nguyễn Văn A");
+//   alert("Xin chào " + ten + "!");
+    
